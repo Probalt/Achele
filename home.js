30fdecd9659f375b51908b79eb250b6f8ff11b39
Balthazar Le Bret <balthazarlebret@AirdeBalthazar.home>
2020-03-11 22:25:52 +0100

    photo

--- home.js
+++ home.js
@@ -1,12 +1,25 @@
 const items = document.querySelectorAll(".accordion a");
+// const actives = document.querySelectorAll(".active");
 
 function toggleAccordion(){
-  $('.active').removeClass('active');
+  $(".active").removeClass('active');
   this.classList.toggle('active');
   this.nextElementSibling.classList.toggle('active');
 }
 
+// function toggleAcco(){
+//   this.classList.toggle('active');
+//   this.nextElementSibling.classList.toggle('active');
+// }
+
+// function toggleActive(){
+//   this.classList.toggle('active');
+//   this.nextElementSibling.classList.toggle('active');	
+// }
+
 items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+// actives.forEach(active => active.addEventListener('click', toggleActive));
 
 // $(function() {
 //   $('a[href*=#]').on('click', function(e) {
