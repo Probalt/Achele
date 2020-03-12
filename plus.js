340aef8b3cb9745ab7149ca7feb208a7a01db041
Balthazar Le Bret <balthazarlebret@AirdeBalthazar.home>
2020-03-12 13:56:16 +0100

    full length site

--- plus.js
+++ plus.js
@@ -1,4 +1,4 @@
-$('.bloc a').click(function () {
+$('.accordion-item a').click(function () {
   $('.plus-minus-toggle').addClass('collapsed');
   $(this).find('.plus-minus-toggle').toggleClass('collapsed');
   // $(this).parent().toggleClass('active');
